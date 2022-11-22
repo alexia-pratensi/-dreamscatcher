8399e21ce9ff918d0fd093289fca7c017c64e5f5
Melanie Armitano <melanie.armi@gmail.com>
2022-11-22 13:03:51 +0100

    new/create action + view

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,2 +1,22 @@
 class DreamsController < ApplicationController
+
+  def new
+    @dream = Dream.new
+  end
+
+  def create
+    @dream = Dream.new(dream_params)
+    @dream.user_id = current_user.id
+    if @dream.save
+      redirect_to @dream, notice: 'Ton rêve a été soumis avec succès !'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def dream_params
+    params.require(:dream).permit(:name, :details, :price, :photo)
+  end
 end
--- app/models/dream.rb
+++ app/models/dream.rb
@@ -1,4 +1,5 @@
 class Dream < ApplicationRecord
   has_many :reservations
   belongs_to :user # loueur
+  has_one_attached :photo
 end
