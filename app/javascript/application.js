7b70e444dbab7b4a7f82124afa1ccd741827de76
Melanie Armitano <melanie.armi@gmail.com>
2022-11-24 17:26:14 +0100

    gem

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,15 +1,4 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-
-import "jquery"
-import "popper"
-import "turbolinks"
 import "bootstrap"
-import "require_tree."
-
-//= require jquery
-//= require popper
-//= require turbolinks
-//= require bootstrap
-//= require_tree.
