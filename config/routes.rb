5860ef4c75ec88c2f14b4e087ae3b7c6f9822b39
alexia pratensi <alexiapratensi@hotmail.fr>
2022-11-25 10:19:00 +0100

    finalisation dashbord

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
 
   root to: "pages#home"
   get '/dashbord', to: "pages#dashbord"
-  delete "dreams/:id", to: "pages#destroy"
+  delete "dreams/:id", to: "pages#destroy", as: :destroy_dream
   # root "articles#index"
   resources :dreams, except: [:show]
 
   resources :dreams, only: [:show] do
     resources :reservations, only: :create
   end
+  resources :reservations, only: :destroy
 end
